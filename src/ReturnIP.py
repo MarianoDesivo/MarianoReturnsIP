79faf6fc6221b44f19bcf1d2c28f9d4972cbc678
Mariano Desivo <79780807+MarianoDesivo@users.noreply.github.com>
2022-03-23 20:13:13 -0300

    Update ReturnIP.py

--- src/ReturnIP.py
+++ src/ReturnIP.py
@@ -1,5 +1,19 @@
-import socket
-hostname = socket.gethostname()
-ip_address = socket.gethostbyname(hostname)
-print(f"Hostname: {hostname}")
-print(f"IP Address: {ip_address}")
+from flask import Flask
+from multiprocessing import Process
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'hello'
+
+@app.route('/startheavy')
+def startHeavyProcess():
+    heavy_process = Process(target=heavyProcess, daemon=True)
+    heavy_process.start()
+    return 'process has started'
+
+
+def heavyProcess():
+    for x in range(1, 100):
+        print(x)
